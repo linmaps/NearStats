b563787cb3db140b767094260bba7b59b9f26065
linmaps <lindsay.deel@gmail.com>
2019-05-23 16:19:09 -0400

    Name changes

--- NearStats.R
+++ NearStats.R
@@ -2,5 +2,7 @@
 # Use gadm for the UK as the boundary layer
 # Convert LandScan Global 2016 to points (using only pixels with pop greater than 0) and only for UK
 # Pull OpenStreetMaps data for the UK - combine both point and polygon Points of Interest into a single point layer
-# Extract all health and public safety facilities from the results OSM POIs for the UK (need to combine Scotland, Wales, England, Northern Ireland)
+# Extract all surveillance cameras from the results OSM POIs for the UK (need to combine Scotland, Wales, England, Northern Ireland)
 
+library(tidyverse)
+library(sf)
